65f6fa96f22673b9bd37e58cf2f1daf8c0ca72c9
danielaztudillo <danzt008@gmail.com>
2016-07-07 18:21:13 -0500

    mail

--- js/custom.js
+++ js/custom.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function() {
 	//this code is for the gmap
 	 var map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
+        lat: 22.2773763,
+        lng: -97.8689805
       });
 
 
